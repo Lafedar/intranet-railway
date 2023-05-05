84e7152c2591b9278d6c1bcab7926d8adc9406d6
Lafedar <agusgonzalez603@hotmail.com>
2023-05-04 22:46:49 -0300

    Mejora de codigo
    *Probar si funciona.

--- app/Http/Controllers/Equipo_mantController.php
+++ app/Http/Controllers/Equipo_mantController.php
@@ -54,15 +54,8 @@ class Equipo_mantController extends Controller
 
     public function show_update_equipo_mant($id)
     {
-        $equipo_mant = DB::table('equipos_mant')
-        ->leftjoin('localizaciones', 'localizaciones.id', 'equipos_mant.id_localizacion')
-        ->leftjoin('area', 'area.id_a', 'localizaciones.id_area')
-        ->leftjoin('tipos_equipos', 'tipos_equipos.id', 'equipos_mant.id_tipo')
-        ->select('equipos_mant.id as id', 'equipos_mant.marca as marca', 'equipos_mant.modelo as modelo', 'equipos_mant.descripcion as descripcion', 
-        'equipos_mant.uso as uso', 'localizaciones.nombre as localizacion', 'area.nombre_a as area', 'equipos_mant.uso as uso', 
-        'tipos_equipos.nombre as nombre_tipo', 'equipos_mant.num_serie as num_serie')
-        ->where('equipos_mant.id', $id)
-        ->first();
+        $equipoMantModel = new EquiposMant();
+        $equipo_mant = $equipoMantModel->getEquipoMantPorId($id);
 
         return view('equipos_mant.update', ['equipo_mant' => $equipo_mant]);       
     }
