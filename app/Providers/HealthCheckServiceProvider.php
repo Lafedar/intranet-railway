0870fd790204af4d106ff2808adf08a43eb76d1a
geroalles <geronimo.alles@lafedar.com>
2024-04-30 11:48:39 -0300

    agregar check de ping

--- app/Providers/HealthCheckServiceProvider.php
+++ app/Providers/HealthCheckServiceProvider.php
@@ -8,6 +8,7 @@ use Spatie\Health\Checks\Checks\DatabaseCheck;
 use Spatie\SecurityAdvisoriesHealthCheck\SecurityAdvisoriesCheck;
 use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
 use Spatie\Health\Checks\Checks\CacheCheck;
+use Spatie\Health\Checks\Checks\PingCheck;
 
 
 
@@ -23,15 +24,17 @@ class HealthCheckServiceProvider extends ServiceProvider
     public function register()
     {
         Health::checks([
-            DatabaseCheck::new(),
-            SecurityAdvisoriesCheck::new(),
-            CacheCheck::new(),
-            DatabaseConnectionCountCheck::new()
+            DatabaseCheck::new(), //verifica conexion a la base de datos. 
+            SecurityAdvisoriesCheck::new(), //verifica si los paquetes PHP tienen vulnerabilidades de seguridad conocidas.
+            CacheCheck::new(), //verifica que la aplicación pueda conectarse a su sistema de caché y leer/escribir en las claves de caché. 
+            PingCheck::new()->url('http://intranet.lafedar.net/')->retryTimes(3),  //enviará una solicitud a una URL determinada. Informará de un error cuando esa URL no responda con un código de respuesta exitosa en un segundo.
+            DatabaseConnectionCountCheck::new() //cantidad de usuarios conectados a la base de datos
         ->warnWhenMoreConnectionsThan(130)
         ->failWhenMoreConnectionsThan(150)
         ]);
         
     }
+
 
     /**
      * Bootstrap services.
