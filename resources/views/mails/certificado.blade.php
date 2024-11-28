1974b4ae9568804e932a1c45fd38d2d5589f0315
geroalles <geronimo.alles@lafedar.com>
2024-11-28 09:19:25 -0300

    cambiar mensaje de mail y agregar foto de firma

--- resources/views/mails/certificado.blade.php
+++ resources/views/mails/certificado.blade.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Certificado de Curso</title>
+    <title>Certificado de Curso</title>
 </head>
+
 <body>
-<p>Hola, {{$nombre}} {{$apellido}}</p>
-<p>Adjunto encontrarás tu certificado del curso: "{{$curso}}" completado.</p>
-<p>¡Felicidades!</p>
+    <p>Hola, {{$nombre}} {{$apellido}}</p>
+    <p>Adjunto encontrarás tu certificado del curso: "{{$curso}}" completado.</p>
+    <p>¡Felicitaciones!</p>
+    <img src="{{ asset('storage/cursos/firma.jpg') }}">
 </body>
+
 </html>
