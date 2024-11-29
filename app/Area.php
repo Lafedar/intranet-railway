39477cb249d2471a2165c8142e8b30958137c9b6
geroalles <geronimo.alles@lafedar.com>
2024-11-29 11:07:07 -0300

    pasar el css a principal-nueva.css

--- app/Area.php
+++ app/Area.php
@@ -3,13 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use DB;
+
 
 class Area extends Model
 {
     public $table = "area";
     public $timestamps = false;
-    public function scopeIndex ($query)
+    public function scopeIndex($query)
     {
         return $query->select('area.id_a as id_a', 'area.nombre_a as nombre_a');
     }
