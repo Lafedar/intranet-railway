13ecd2628b71235d9e6fba5524f63bc86bff4a91
geroalles <geronimo.alles@lafedar.com>
2024-12-05 16:47:24 -0300

    dar orden a layouts.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,47 +1,50 @@
 <!DOCTYPE html>
 <html lang="es">
 
-
-
-<link href="{{ URL::asset('/css/bootstrap.min.css') }}" rel="stylesheet" id="bootstrap-css">
-
-<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-
-
-<script type="text/javascript" src="{{ URL::asset('/js/modal-popper.min.js') }}"></script>
-<script type="text/javascript" src="{{ URL::asset('/js/modal-bootstrap.min.js') }}"></script>
-
 <head>
-
 	<meta charset="UTF-8">
-
 	<title>Intranet Lafedar</title>
-
-	<link rel="icon" href="img/ico.png" type="image/png" />
-
+	<link rel="icon" href="img/ico.png" type="image/png">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<header class="page-header">
-		<div class="logo">
-			<a href="{{ route('home.inicio') }}">
-				<img src="{{ asset('storage/Imagenes-principal-nueva/LOGO-LAFEDAR.png') }}" alt="Logo de la empresa"
-					style="margin-left: 0px;">
-			</a>
-		</div>
+	<!-- Bootstrap -->
+	<link href="{{ URL::asset('/css/bootstrap.min.css') }}" rel="stylesheet" id="bootstrap-css">
 
-		<div class="menu">
-		</div>
-	</header>
-	</nav>
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+
+	<!-- Scripts -->
+	<script type="text/javascript" src="{{ URL::asset('/js/modal-popper.min.js') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('/js/modal-bootstrap.min.js') }}"></script>
 </head>
 
-<body>
-	<div class="row">
-		@yield('content')
-	</div>
+<body class="d-flex flex-column min-vh-100">
+	<!-- Header -->
+	<header class="page-header ">
+		<div>
+			<div class="logo">
+				<a href="{{ route('home.inicio') }}">
+					<img src="{{ asset('storage/Imagenes-principal-nueva/LOGO-LAFEDAR.png') }}" alt="Logo de la empresa"
+						>
+				</a>
+			</div>
+			
+		</div>
+	</header>
+
+	<!-- Main content -->
+	<main class="flex-grow-1">
+		<div class="container py-4">
+			<div class="row">
+				@yield('content')
+			</div>
+		</div>
+	</main>
+
+	<!-- Footer -->
+	<footer>
+		<p>Laboratorio Lafedar S.A. | Laboratorios Federales Argentinos S.A</p>
+	</footer>
 </body>
-<footer>
-	<p>Laboratorio Lafedar S.A. | Laboratorios Federales Argentinos S.A </p>
-</footer>
 
 </html>
