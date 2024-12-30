e80d1076a96b4363b0541ad2888eb12416b9b8e3
geroalles <geronimo.alles@lafedar.com>
2024-12-30 08:24:42 -0300

    agregar relacion en enrolamiento_cursos y marcar completos los cursos viejos

--- app/Models/CursoInstancia.php
+++ app/Models/CursoInstancia.php
@@ -14,18 +14,18 @@ class CursoInstancia extends Model
 
     //el campo id de la tabla cursos_instancia lo reconoce Laravel automaticamente como primary key de la tabla
     protected $fillable = [
-        'id_curso', 
-        'fecha_inicio', 
-        'fecha_fin', 
-        'cupo', 
-        'modalidad', 
-        'capacitador', 
-        'lugar', 
+        'id_curso',
+        'fecha_inicio',
+        'fecha_fin',
+        'cupo',
+        'modalidad',
+        'capacitador',
+        'lugar',
         'estado',
         'version',
         'id_instancia'
 
-        
+
     ];
 
     // Indicar a Laravel que los campos son de tipo fecha
@@ -33,7 +33,7 @@ class CursoInstancia extends Model
         'fecha_inicio',
         'fecha_fin',
     ];
-   
+
     public $timestamps = true;
 
     public function curso()
@@ -41,23 +41,28 @@ class CursoInstancia extends Model
         return $this->belongsTo(Curso::class, 'id_curso');
     }
 
-    
-    public function enrolamientos()
+
+    /*public function enrolamientos()
 {
     return $this->hasMany(EnrolamientoCurso::class, 'id_instancia', 'id_instancia');
-}
-
-/*public function anexos()
+}*/
+    public function enrolamientos()
     {
-        return $this->belongsToMany(Anexo::class, 'relacion_curso_instancia_anexo', 'id_curso', 'formulario_id')
-                    ->withPivot('id_instancia', 'tipo'); // Incluye columna adicional de la tabla intermedia
-    }*/
+        return $this->hasMany(EnrolamientoCurso::class, 'id_instancia', 'id_instancia')
+            ->where('id_curso', $this->id_curso);  // Asegúrate de que 'id_curso' esté presente en CursoInstancia
+    }
+
+    /*public function anexos()
+        {
+            return $this->belongsToMany(Anexo::class, 'relacion_curso_instancia_anexo', 'id_curso', 'formulario_id')
+                        ->withPivot('id_instancia', 'tipo'); // Incluye columna adicional de la tabla intermedia
+        }*/
     public function anexos()
     {
         return $this->belongsToMany(Anexo::class, 'relacion_curso_instancia_anexo', 'id_instancia', 'formulario_id')
-                    ->withPivot('id_curso'); // Esto asegura que 'id_curso' esté accesible
+            ->withPivot('id_curso'); // Esto asegura que 'id_curso' esté accesible
     }
 
 
-    
+
 }
