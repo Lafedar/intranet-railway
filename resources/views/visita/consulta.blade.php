982e1ac78e9fd3d9757d153e0ed2548d5814118c
geroalles <geronimo.alles@lafedar.com>
2025-02-14 15:09:15 -0300

    sacar bootstrap repetidos en la misma vista

--- resources/views/visita/consulta.blade.php
+++ resources/views/visita/consulta.blade.php
@@ -1,42 +1,41 @@
 @extends('layouts.app')
 
 @push('styles')
-
-<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 @endpush
 
 @section('content')
-<div id="guardia-consulta-container">
-  <h1>
+  <div id="guardia-consulta-container">
+    <h1>
     <div class="form-inline pull-right">
       <form method="GET">
-        <div class="form-group">
-          <label>
-            <h6><b>Tarjeta:</b></h6>
-          </label>
-          <input type="text" name="tarjeta" class="form-control col-md-1" id="tarjeta" value="{{$tarjeta}}">
-          &nbsp
-          <label>
-            <h6><b>Visitante:</b></h6>
-          </label>
-          <input type="text" name="visitante" class="form-control col-md-2" id="visitante" value="{{$visitante}}">
-          &nbsp
-          <label>
-            <h6><b>Visita a:</b></h6>
-          </label>
-          <input type="text" name="visita_a" class="form-control col-md-2" id="visita_a" value="{{$visita_a}}">
-          &nbsp
-          <label>
-            <h6><b>Fecha:</b></h6>
-          </label>
-          <input type="date" name="fecha" class="form-control" step="1" min="2019-01-01" value="{{$fecha}}">
-          &nbsp
-          <select class="form-control" name="estado" id="estado" value="">
-            @if($estado == 2 || $estado == null)
-        <option value="2" selected>{{'Todas'}} </option>
-        <option value="1">{{'Activas'}} </option>
-        <option value="0">{{'Finalizadas'}} </option>
-      @elseif($estado == 1)
+      <div class="form-group">
+        <label>
+        <h6><b>Tarjeta:</b></h6>
+        </label>
+        <input type="text" name="tarjeta" class="form-control col-md-1" id="tarjeta" value="{{$tarjeta}}">
+        &nbsp
+        <label>
+        <h6><b>Visitante:</b></h6>
+        </label>
+        <input type="text" name="visitante" class="form-control col-md-2" id="visitante" value="{{$visitante}}">
+        &nbsp
+        <label>
+        <h6><b>Visita a:</b></h6>
+        </label>
+        <input type="text" name="visita_a" class="form-control col-md-2" id="visita_a" value="{{$visita_a}}">
+        &nbsp
+        <label>
+        <h6><b>Fecha:</b></h6>
+        </label>
+        <input type="date" name="fecha" class="form-control" step="1" min="2019-01-01" value="{{$fecha}}">
+        &nbsp
+        <select class="form-control" name="estado" id="estado" value="">
+        @if($estado == 2 || $estado == null)
+      <option value="2" selected>{{'Todas'}} </option>
+      <option value="1">{{'Activas'}} </option>
+      <option value="0">{{'Finalizadas'}} </option>
+    @elseif($estado == 1)
     <option value="2">{{'Todas'}} </option>
     <option value="1" selected>{{'Activas'}} </option>
     <option value="0">{{'Finalizadas'}} </option>
@@ -45,29 +44,29 @@
   <option value="1">{{'Activas'}} </option>
   <option value="0" selected>{{'Finalizadas'}} </option>
 @endif
-          </select>
-          &nbsp
-          <button type="submit" class="btn btn-default" id="asignar-btn"> Buscar</button>
+        </select>
+        &nbsp
+        <button type="submit" class="btn btn-default" id="asignar-btn"> Buscar</button>
       </form>
     </div>
-  </h1>
+    </h1>
 
 
-  <div>
+    <div>
     <table>
       <thead>
-        <th class="text-center">Tarjeta</th>
-        <th class="text-center">Visitante</th>
-        <th class="text-center">Empresa</th>
-        <th class="text-center">Visita a</th>
-        <th class="text-center">Fecha</th>
-        <th class="text-center">Desde</th>
-        <th class="text-center">Hasta</th>
-        <th class="text-center">Estado</th>
-        <th class="text-center">Acciones</th>
+      <th class="text-center">Tarjeta</th>
+      <th class="text-center">Visitante</th>
+      <th class="text-center">Empresa</th>
+      <th class="text-center">Visita a</th>
+      <th class="text-center">Fecha</th>
+      <th class="text-center">Desde</th>
+      <th class="text-center">Hasta</th>
+      <th class="text-center">Estado</th>
+      <th class="text-center">Acciones</th>
       </thead>
       <tbody>
-        @if(count($visitas))
+      @if(count($visitas))
       @foreach($visitas as $visita) 
       <tr>
       <td align="center" width="80">{{$visita->tarjeta}}</td>
@@ -92,7 +91,7 @@
     @endif
       <td align="center" width="80">
       <a href="#" data-toggle="modal" data-dni="{{$visita->dni_ext}}" data-target="#foto_externo" type="submit"
-        title="Ver Foto"><img src="{{ asset('storage/cursos/foto.png') }}" alt="Ver Foto" id="img-icono"></a>
+      title="Ver Foto"><img src="{{ asset('storage/cursos/foto.png') }}" alt="Ver Foto" id="img-icono"></a>
       </td>
       </tr>
     @endforeach
@@ -101,19 +100,19 @@
     </table>
 
     {{ $visitas->links('pagination::bootstrap-4') }} <!--paginacion-->
+    </div>
+    @include('visita.modal_foto_externo')
   </div>
-  @include('visita.modal_foto_externo')
-</div>
 
 
 @endsection
 @push('scripts')
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script> 
-<script>
-  $('#foto_externo').on('show.bs.modal', function (event) {
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  <script>
+    $('#foto_externo').on('show.bs.modal', function (event) {
     document.getElementById("ver_foto").src = " ";
     var button = $(event.relatedTarget);
     var dni = button.data('dni');
@@ -124,6 +123,6 @@
       storage = storage.replace(':fotito_reemplaza', data[0].foto);
       foto = document.getElementById("ver_foto").src = storage;
     });
-  })
-</script>
+    })
+  </script>
 @endpush
--- resources/views/visita/listado.blade.php
+++ resources/views/visita/listado.blade.php
@@ -79,8 +79,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="{{ URL::asset('/js/bootstrap.min.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
 
 <script>
   $('#editar_externo').on('show.bs.modal', function (event) {
