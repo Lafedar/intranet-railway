5c799b33d49a176298881209e999f0cf74b5f120
geroalles <geronimo.alles@lafedar.com>
2025-02-18 08:50:18 -0300

    sacar AuthenticatesUsers innecesario

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Services\NovedadService;
@@ -29,7 +28,7 @@ class LoginController extends Controller
 
     }
 
-    use AuthenticatesUsers;
+
 
     protected $redirectTo = '/';
 
@@ -39,7 +38,7 @@ class LoginController extends Controller
         return '/';
     }
 
-    use AuthenticatesUsers;
+   
 
     // Método para mostrar el formulario de inicio de sesión
     public function showLoginForm()
