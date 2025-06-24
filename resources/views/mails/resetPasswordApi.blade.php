8d34d3aeff80ae045809cc1830e9e0acbb819588
geroalles <geronimo.alles@lafedar.com>
2025-06-24 12:29:21 -0300

    agregar title al mail de restablecimiento de contraseña

--- resources/views/mails/resetPasswordApi.blade.php
+++ resources/views/mails/resetPasswordApi.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Restablecimiento de Contraseña</title>
 </head>
 
 <body>
@@ -12,7 +12,7 @@
     <p>Hemos recibido una solicitud de restablecimiento de contraseña para tu cuenta. Para restablecer tu contraseña, hacé clic en el siguiente enlace:</p>
 
     <a href="{{ url('/redirectToResetPassword/' . $token) }}" target="_blank" rel="noopener noreferrer">Restablecer contraseña</a>
-    
+
     <p><strong>Este enlace es válido por 24 horas.</strong></p>
     
     <p>Si no fuiste vos, ignorá este correo.</p>
