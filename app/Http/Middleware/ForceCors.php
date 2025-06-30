593f8d0e321059f5833ec0cdeacc2212ea8c13bc
Alejandro <alejandro.cabrera@lafedar.com>
2025-06-30 16:05:46 -0300

    add log cors file

--- app/Http/Middleware/ForceCors.php
+++ app/Http/Middleware/ForceCors.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ForceCors
 {
@@ -31,6 +32,7 @@ class ForceCors
 
         // Manejar preflight request
         if ($request->getMethod() === 'OPTIONS') {
+            Log::info('ForceCors middleware OPTIONS request origen: ' . $origin);
             return response('', 204)
                 ->header('Access-Control-Allow-Origin', $allowOrigin)
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
