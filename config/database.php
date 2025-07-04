b364afd876d96f58657169fdf85c0ae48e86ca2b
geroalles <geronimo.alles@lafedar.com>
2025-07-04 09:57:13 -0300

    agregar conexion mysql en config/database

--- config/database.php
+++ config/database.php
@@ -39,8 +39,23 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'fake'),
+            'username' => env('DB_USERNAME', 'fake'),
+            'password' => env('DB_PASSWORD', 'fake'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
 
-    
+
         'mysql_read' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
