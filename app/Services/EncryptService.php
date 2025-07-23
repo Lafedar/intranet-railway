4e771fcb6504ed5d89d9a881daab3f801cdc6bee
geroalles <geronimo.alles@lafedar.com>
2025-07-23 15:00:50 -0300

    no decodificar aesKey

--- app/Services/EncryptService.php
+++ app/Services/EncryptService.php
@@ -9,44 +9,40 @@ use Request;
 class EncryptService
 {
     // Servicio - solo lógica
-    public function decrypt($request, $headerAesKey)
+    public function decrypt($request, $aesKeyHeader)
     {
         try {
             $ciphertextBase64 = $request->input('ciphertext');
             $ivBase64 = $request->input('iv');
 
-            if (!$ciphertextBase64 || !$ivBase64 || !$headerAesKey) {
+            if (!$ciphertextBase64 || !$ivBase64)
                 return null;
-            }
 
             $ciphertext = base64_decode($ciphertextBase64);
             $iv = base64_decode($ivBase64);
-            $aesKey = base64_decode($headerAesKey);
+           
 
-            if (strlen($ciphertext) < 16 || strlen($aesKey) !== 32 || strlen($iv) !== 12) {
-                Log::warning('Datos mal formateados en decrypt');
+            $aesKey = $aesKeyHeader;
+            $tagLength = 16;
+            if (strlen($ciphertext) < $tagLength)
                 return null;
-            }
 
-            $tag = substr($ciphertext, -16);
-            $ciphertextRaw = substr($ciphertext, 0, -16);
+            $tag = substr($ciphertext, -$tagLength);
+            $ciphertextRaw = substr($ciphertext, 0, -$tagLength);
 
-            $decrypted = openssl_decrypt(
+            return openssl_decrypt(
                 $ciphertextRaw,
                 'aes-256-gcm',
                 $aesKey,
                 OPENSSL_RAW_DATA,
                 $iv,
                 $tag
-            );
-
-            return $decrypted ?: null;
+            ) ?: null;
         } catch (Exception $e) {
-            Log::error('Error decrypting data: ' . $e->getMessage());
-            return null;
+            Log::error('Error in class: ' . __CLASS__ . ' - Method: ' . __FUNCTION__ . ' - Error decrypting data: ' . $e->getMessage());
+
         }
     }
-
     public function decryptFile(array $payload, $aesKeyHeader): ?string
     {
         try {
